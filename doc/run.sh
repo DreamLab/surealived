c41f7ad08a8f5d086510c5150ef193009526930c
Bartosz Ponurkiewicz <ponury@gmail.com>
2009-10-22 17:17:23 +0000

    - complete translation - fixed bugs in documentation

--- doc/run.sh
+++ doc/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-./makelatex.pl *.doc && latex surealived_??.tex && latex surealived_??.tex
+./makelatex.pl *.doc && latex surealived_PL.tex && latex surealived_PL.tex
+latex surealived_EN.tex && latex surealived_EN.tex
 dvipdf surealived_PL.dvi
 dvipdf surealived_EN.dvi
-xpdf surealived_EN.pdf
+xpdf surealived_EN.pdf &
+xpdf surealived_PL.pdf
 
